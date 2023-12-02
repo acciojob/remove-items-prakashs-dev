546704b96124a4157fcda5ac93c42a8a5b998ee1
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-02 09:59:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+      const select = document.getElementById("colorSelect");
+      const button = document.querySelector("input");
+      function remove() {
+        select.remove(select.selectedIndex);
+      }
+      button.addEventListener("click", remove);
